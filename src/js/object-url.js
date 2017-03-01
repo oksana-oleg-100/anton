33a28288c68d88ae374f39c9be7a904bd03c72df
olegsas <o.sas@aisnovations.com>
2017-03-01 23:28:42 +0300

    store styles in style.css file

---
+++ src/js/object-url.js
@@ -0,0 +1,50 @@
+<script type="text/javascript">
+    
+    var publicModel = {
+        "domain":"daineko.net",
+        "externalBaseUrl":"http:\/\/www.daineko.net\/",
+        "unicodeExternalBaseUrl":"http:\/\/www.daineko.net\/",
+        "pageList":{"pages":
+            [{"pageId":"hkz6k",
+            "title":"ICON",
+            "pageUriSEO":"icon",
+            "pageJsonFileName":"419bbe_86b7586ca330d03d8e303397c83950ae_280.json"},
+            {"pageId":"c24ol",
+            "title":"ARTICLES",
+            "pageUriSEO":"articles",
+            "pageJsonFileName":"419bbe_5587d1420441e79606cd36c970ba2e0e_280.json"},
+            {"pageId":"lq8l1",
+            "title":"Духовно-образовательный Центр",
+            "pageUriSEO":"blank-lq8l1",
+            "pageJsonFileName":"419bbe_61b28f4344dce9e89ef8abc7c51652fc_280.json"},
+            {"pageId":"czpl",
+            "title":"CONTACT",
+            "pageUriSEO":"contact",
+            "pageJsonFileName":"419bbe_3b2edc42b81dd368dc9e275a42dd37a4_280.json"},
+            {"pageId":"zh0ij",
+            "title":"ХрамВознесения",
+            "pageUriSEO":"blank-zh0ij",
+            "pageJsonFileName":"419bbe_a71fe8ceeddd27f407425853f38719e0_282.json"},
+            {"pageId":"g1o73",
+            "title":"Храм Архангела Михаила",
+            "pageUriSEO":"blank-g1o73",
+            "pageJsonFileName":"419bbe_caf823ae2bc98c2e8b8756cb6b435d07_280.json"},
+            {"pageId":"c17b1",
+            "title":"GALLERY",
+            "pageUriSEO":"gallery",
+            "pageJsonFileName":"419bbe_fae165468e4eafa247fdfe4f208a678e_280.json"},
+            {"pageId":"c136u",
+            "title":"ABOUT",
+            "pageUriSEO":"about",
+            "pageJsonFileName":"419bbe_8bc1e5b601796310cd84e5cdd7402154_280.json"},
+            {"pageId":"xzpwp",
+            "title":"иконостасы",
+            "pageUriSEO":"blank-xzpwp",
+            "pageJsonFileName":"419bbe_40904c9034fd6b8cbce3c4120e23e977_280.json"},
+            {"pageId":"cv8pn","title":"Храм Воскрешения Праведного Лазаря","pageUriSEO":"blank-cv8pn","pageJsonFileName":"419bbe_088d316a9cc9b6c49c4375b26e5ba1d6_280.json"},{"pageId":"mainPage","title":"HOME","pageUriSEO":"home","pageJsonFileName":"419bbe_2bb1752e7fc8d8057a292b64562984c3_282.json"},{"pageId":"skuau","title":"иконы","pageUriSEO":"blank-skuau","pageJsonFileName":"419bbe_053ded35adfeaf08714b8495318f3893_280.json"},{"pageId":"cvxo","title":"FRESCO","pageUriSEO":"fresco","pageJsonFileName":"419bbe_8061bb574a8af729a3e76abcd63c836c_280.json"}],"mainPageId":"mainPage","masterPageJsonFileName":"419bbe_e6d4d107fe33e6abe2d58b6418459e72_282.json","topology":[{"baseUrl":"https:\/\/static.wixstatic.com\/","parts":"sites\/{filename}.z?v=3"},{"baseUrl":"https:\/\/staticorigin.wixstatic.com\/","parts":"sites\/{filename}.z?v=3"},{"baseUrl":"https:\/\/www.daineko.net\/","parts":"sites\/{filename}.z?v=3"},{"baseUrl":"https:\/\/fallback.wix.com\/","parts":"wix-html-editor-pages-webapp\/page\/{filename}"}]},"timeSincePublish":6054186837,"favicon":"419bbe_8634cb503b40447e95344a411a7f2543.jpg","deviceInfo":{"deviceType":"Desktop","browserType":"Chrome","browserVersion":56},"siteRevision":282,"sessionInfo":{"hs":-1388822528,"svSession":"1ad7fb252ff5e2aeee88a57059e987f544522ff03586eaea43dd5cb30eccf8135d45e41039e975d89b84075d4400b5541e60994d53964e647acf431e4f798bcdc3db2086ca94acfa01913063f15763532a76ff6b0c08256e9d7fdc4b28c8cf9b","ctToken":"SXJhYlJ2Y0VUX0VGQVczQ2RYTVkwSUZNRGVWUlRnUU1IOExQanlHUHBnUXx7InVzZXJBZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDYuMSkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzU2LjAuMjkyNC44NyBTYWZhcmkvNTM3LjM2IiwidmFsaWRUaHJvdWdoIjoxNDg4OTI2NDM0ODQzfQ","isAnonymous":false},"metaSiteFlags":[],"indexable":true};
+
+    
+
+    
+
+</script>
